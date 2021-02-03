50bd03dd76108fd81a166ad4749ad896f78447ca
markcool17 <45372340+markcool17@users.noreply.github.com>
2021-02-03 20:21:48 +0200

    Добавьте файлы проекта.

---
+++ ITCraftTrainee/ClientApp/src/app/app.component.ts
@@ -0,0 +1,78 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { DataService } from './data.service';
+import { User } from './user';
+
+@Component({
+    selector: 'app',
+    templateUrl: './app.component.html',
+    providers: [DataService]
+})
+export class AppComponent implements OnInit {
+   
+    user: User = new User();   
+    users: User[];               
+    tableMode: boolean = true;
+    activeUsersCount = 0;
+    UsersCount = 0;
+
+    constructor(private dataService: DataService) { }
+
+    ngOnInit() {
+        this.loadUsers();    
+    }
+    loadUsers() {
+        this.dataService.getUsers()
+            .subscribe((data: User[]) => this.users = data);
+    }
+    save() {
+        if (this.user.id == null) {
+            this.dataService.createUser(this.user)
+                .subscribe((data: User) => this.users.push(data));
+        } else {
+            this.dataService.updateUser(this.user)
+                .subscribe(data => this.loadUsers());
+        }
+        this.cancel();
+    }
+    changeActivity(u: User) {
+        this.user = u;
+        if (this.user.active == false) {
+            this.user.active = true;
+            this.dataService.updateUser(this.user)
+                .subscribe(data => this.loadUsers());
+        }
+        else {
+            this.user.active = false;
+            this.dataService.updateUser(this.user)
+                .subscribe(data => this.loadUsers());
+        }
+        this.cancel();
+        
+    }
+    
+    cancel() {
+        this.user = new User();
+        this.tableMode = true;
+    }
+    delete(u: User) {
+        this.dataService.deleteUser(u.id)
+            .subscribe(data => this.loadUsers());
+    }
+    popupappear() {
+        this.dataService.getUsers()
+            .subscribe((data: User[]) => this.users = data);
+        this.UsersCount = this.users.length;
+        for (let u of this.users) {
+            if (u.active == true)
+                this.activeUsersCount += 1;
+        }
+        this.cancel();
+        this.tableMode = false;
+    }
+    closepopup() {
+        this.activeUsersCount = 0;
+        this.cancel();
+        this.tableMode = true;
+    }
+}
---
+++ ITCraftTrainee/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using ITCraftTrainee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITCraftTrainee.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        ApplicationContext db;
+        public UserController(ApplicationContext context)
+        {
+            db = context;
+            if (!db.Users.Any())
+            {
+                db.Users.Add(new User { Name = "Tanyia",  Active = false });
+                db.Users.Add(new User { Name = "Igor", Active = false });
+                db.Users.Add(new User { Name = "Eugene", Active = false });
+                db.Users.Add(new User { Name = "Tanyia", Active = false });
+                db.Users.Add(new User { Name = "Igor", Active = false });
+                db.Users.Add(new User { Name = "Olga", Active = false });
+                db.Users.Add(new User { Name = "Victor", Active = false });
+                db.Users.Add(new User { Name = "Igor", Active = false });
+                db.Users.Add(new User { Name = "Eugene", Active = false });
+                db.Users.Add(new User { Name = "Nikolay", Active = false });
+                db.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            User User = db.Users.FirstOrDefault(x => x.Id == id);
+            return User;
+        }
+
+        [HttpPost]
+        public IActionResult Post(User User)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(User);
+                db.SaveChanges();
+                return Ok(User);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public IActionResult Put(User User)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(User);
+                db.SaveChanges();
+                return Ok(User);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            User User = db.Users.FirstOrDefault(x => x.Id == id);
+            if (User != null)
+            {
+                db.Users.Remove(User);
+                db.SaveChanges();
+            }
+            return Ok(User);
+        }
+    }
+}
